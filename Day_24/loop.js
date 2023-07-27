556460f441646f74f0ecdb62c1da89315b6c70bc
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-07-27 22:28:41 +0530

    JavaScript (Looping, object,function)

---
+++ Day_24/loop.js
@@ -0,0 +1,23 @@
+let i;
+// For Loop
+console.log('For Loop');
+for ( i = 0; i <= 5 ;i++){
+    console.log(i)
+5  }
+
+console.log('While Loop');
+
+// while Loop
+let x = 1;    // initailzation
+while (x <=10) {
+    console.log(x);
+    x++;
+}
+// Do-while Loop
+console.log('Do-While Loop');
+let a=1;
+
+do{
+console.log(a);
+a=a+2;
+}while(a<=10);
---
+++ Day_24/object.js
@@ -0,0 +1,63 @@
+// let rectangle = {
+//     length:10,
+//     breadth: 12,
+
+// // function
+//     draw: function() {
+//         console.log("Drawing Rectangle");
+//     }
+// }
+
+// function createRectangle() {
+//     return rectangle ={
+//         length:10,
+//         breadth:12,
+//         draw(){
+//             console.log("Drawing Rectangle");
+//         }
+//     };
+// }
+// rectangle.breadth // use breadth
+// rectangle.length // use lenght
+// rectangle.draw() // use draw
+
+// let a = createRectangle();
+
+// a = {
+//     length
+// }
+
+// User Input
+function crtRectangle(len,brd) {
+    return rectangle ={
+        length: len,
+        breadth:brd,
+        draw(){
+            console.log("Drawing Rectangle");
+        }
+    };
+}
+
+let obj = crtRectangle(4,5);
+let obj1 = crtRectangle(6,5);
+let obj2 = crtRectangle(10,5);
+
+function FncPeople(name,age){
+    return People ={
+        Name: name,
+        Age: age,
+        dob(){
+            console.log(name,age);
+        }
+    };
+
+}
+let Student1 = FncPeople('Aditya',20);
+
+function Branch(stdID,sName,branch){
+        this.studentID= stdID;
+        this.studentName = sName;
+        this.branch = branch;
+    }
+
+let student2 = new FncPeople('E212220','Ravi','CSE',)
