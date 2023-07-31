a5a31166d81cc8fea6e6131919bbf72c81e1f3be
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-07-31 22:06:11 +0530

    mathObject String

---
+++ Day_27/stringType.js
@@ -0,0 +1,24 @@
+// Object type
+let firstName = new String('Aditya');
+
+// Primitive type
+let lastName = 'Mathur';
+
+
+console.log(lastName.length);
+console.log(lastName.toLocaleLowerCase());
+console.log(lastName.toLocaleUpperCase());
+
+console.log('Last Name Start with :',lastName.startsWith('Ma'));
+
+let str = 'ji haa mein ho khalnayak';
+
+let words = str.split(' ');
+console.log(words);
+console.log(words.includes ());
+
+// Templete Notation 
+let message = `My Name
+ Is DDBS
+ Hey Boys`;
+ console.log(message);
