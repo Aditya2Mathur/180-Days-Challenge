0309a441ffbf244fbc86d8db9356d95cbad59bf9
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-01 19:54:07 +0530

    iteration & join,split

---
+++ Day_29/iteraingArray.js
@@ -0,0 +1,14 @@
+let arr = [10,20, 30, 40, 50, 60];
+
+//  for-of loop
+for(let value of arr){
+    // console.log(value);
+}
+
+// forEach Function
+let arr1 = [20,30,40,50,60,70];
+arr1.forEach(function(num){
+    console.log(num);
+});
+// arrow notation
+arr1.forEach(num =>console.log(num));
---
+++ Day_29/joinSplit.js
@@ -0,0 +1,7 @@
+let message = 'Bgmi is Not a Game Only';
+//  
+let parts = message.split(' ');
+console.log(parts);
+// joined method 
+let joined = parts .join('_');
+console.log(joined);
