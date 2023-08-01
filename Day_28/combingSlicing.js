a4b5ccddcd0f82cd0c844c2be548f04e3595a7aa
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-01 19:15:38 +0530

    Conbing Empyty array

---
+++ Day_28/combingSlicing.js
@@ -0,0 +1,8 @@
+let first = [1,2,3,4,5,6];
+let second = [7,8,9,0];
+
+let final = first.concat(second);
+
+// slicing
+final.slice(3,7);
+console.log(final);
--- Day_28/emptyArray.js
+++ Day_28/emptyArray.js
@@ -1,4 +1,7 @@
 let emp = [1,3,4,3,2,4,22,2222];
-
+// method - 1
 emp.length=0;
 console.log(emp);
+
+ // method 2 
+ emp.splice(0,emp.length);
