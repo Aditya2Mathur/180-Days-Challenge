38b041d77cc6e549337368823f19ac3b4fcfe196
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-02 22:35:50 +0530

    restOperator & revetionObjectCloning

---
+++ Day_30/restOperator.js
@@ -0,0 +1,4 @@
+function createArray(frist, second,...args){
+    console.log(args);
+}
+createArray('a',2,3,'d',5,6,'h');
---
+++ Day_30/revetionObjectCloning.js
@@ -0,0 +1,10 @@
+// iteraion Method
+let people = {
+    name: 'Addy',
+    age: 20
+}
+for(let key in Object.entries(people)){
+    console.log(people,key);
+}
+// assign  Method
+let student 
