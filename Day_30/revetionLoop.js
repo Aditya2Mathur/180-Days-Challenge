8f73fd7d88aa0509d6acd5e4f6148c98683e72cb
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-02 14:48:31 +0530

    revetion Loop

---
+++ Day_30/revetionLoop.js
@@ -0,0 +1,16 @@
+// for Loop
+for (let index = 0; index <= 100; index = index +2) {
+    console.log(index);    
+}
+// while Loop
+let x = 0;
+while (x <= 10) {
+    console.log(x);
+    x= x+2;
+}
+// Do while Loop
+let a = 3;
+do{
+    console.log(a);
+    a= a+3;
+}while(a <= 30 );
