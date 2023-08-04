196836364836beaf298380d6281a8903d6f284d3
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-05 01:04:00 +0530

    Day 32 changingCSS events

---
+++ Day_32/changeCssByJS.js
@@ -0,0 +1,12 @@
+// document.addEventListener('click', function(){
+//     console.log('Kaam ho gaya');
+// })
+
+// let bro = document.querySelector('#cc').addEventListener('click', function(){
+//     bro.style.color='red';
+// })
+
+const brother = document.querySelector('#days');
+brother.addEventListener('click', function(event){
+    console.log(event); 
+}  )
