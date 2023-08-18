51367846711a1846ba8e8d90f9d0682f7e9725f3
Aditya2Mathur <adiasfreelancer@gmail.com>
2023-08-18 21:39:40 +0530

    java Scipt done using anite.css for text animation

--- Day_45/script.js
+++ Day_45/script.js
@@ -1,87 +1,116 @@
-let boxes = document.querySelectorAll('.box');
-let gameInfo = document.querySelector('.game-info');
-let btn = document.querySelector("btn");
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
 
 let currentPlayer;
 let gameGrid;
-let wimPosition = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
 
-    [2, 5, 8],
-    [1, 4, 7],
-    [0, 3, 6],
-
-    [0, 4, 8],
-    [2, 4, 7],
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ];
-// starting value
+
+//let's create a function to initialise the game
 function initGame() {
     currentPlayer = "X";
-    gameGrid = ["", "", "",
-        "", "", "",
-        "", "", ""];
-    // btn.classList.remove("active");
+    gameGrid = ["","","","","","","","",""];
+    //UI pr empty bhi karna padega boxes ko
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        //one more thing is missing, initialise box with css properties again
+        box.classList = `box box${index+1}`;
+    });
+    newGameBtn.classList.remove("active");
     gameInfo.innerText = `Current Player - ${currentPlayer}`;
-    console.log(`Current Player - ${currentPlayer}`);
 }
+
 initGame();
-// Swap turn ke liye function 
-function swapTurn(){
-    if(currentPlayer === "X"){
+
+function swapTurn() {
+    if(currentPlayer === "X") {
         currentPlayer = "O";
     }
-    else{
+    else {
         currentPlayer = "X";
     }
+    //UI Update
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
-// function checkGameOver(){
-//      let answer;
-//      wimPosition.forEach((position) => {
-//         if(gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "")
-//          (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[2]] === gameGrid[position[1]]){
-//             if(gameGrid[position[0]]  === "X"){
-//                 answer = "X";
-//             }
-//             else{
-//                 answer("O");
-//             }
 
-//             box.forEach((box) =>{
-//                 box.style.pointerEvents = "none";
-//             })
-//             // boxes[position[0]].classList.add(".win")
-//             // boxes[position[1]].classList.add(".win")
-//             // boxes[position[2]].classList.add(".win")
-//             if (answer !== "") {
-//                 gameInfo.innerText = `WINNER PLAYER - ${answer}`;
-//                 return;
-//             }
-//         }
+function checkGameOver() {
+    let answer = "";
 
-//     });
-   
+    winningPositions.forEach((position) => {
+        //all 3 boxes should be non-empty and exactly same in value
+        if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
+            && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
 
+                //check if winner is X
+                if(gameGrid[position[0]] === "X") 
+                    answer = "X";
+                else {
+                    answer = "O";
+                } 
+                    
 
-// }
-// handle CLick function 
-//  click karne ke baad kya kya karna hai
-function handleClick(index){
-    if(gameGrid[index] === ""){
+                //disable pointer events
+                boxes.forEach((box) => {
+                    box.style.pointerEvents = "none";
+                })
+
+                //now we know X/O is a winner
+                boxes[position[0]].classList.add("win");
+                boxes[position[1]].classList.add("win");
+                boxes[position[2]].classList.add("win");
+            }
+    });
+
+    //it means we have a winner
+    if(answer !== "" ) {
+        gameInfo.innerText = `Winner Player - ${answer}`;
+        newGameBtn.classList.add("active");
+        return;
+    }
+
+    //We know, NO Winner Found, let's check whether there is tie
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box !== "" )
+            fillCount++;
+    });
+
+    //board is Filled, game is TIE
+    if(fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+
+}
+
+function handleClick(index) {
+    if(gameGrid[index] === "" ) {
         boxes[index].innerText = currentPlayer;
         gameGrid[index] = currentPlayer;
         boxes[index].style.pointerEvents = "none";
+        //swap karo turn ko
         swapTurn();
+        //check koi jeet toh nahi gya
         checkGameOver();
-        
     }
 }
-// boxes par event listener
-boxes.forEach((box,index) =>{
+
+boxes.forEach((box, index) => {
     box.addEventListener("click", () => {
         handleClick(index);
-    });
+    })
 });
 
-// btn.addEventListener("click", initGame) 
+newGameBtn.addEventListener("click", initGame);
