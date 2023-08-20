63191f2b35e2d780db8496853c31ecf61ec600b2
aditya2mathur <adiasfreelancer@gmail.com>
2023-08-20 22:29:45 +0530

    Create and Fetch Components

---
+++ Day_48/react/src/components/Name.js
@@ -0,0 +1,14 @@
+import "./Name.css";
+
+
+function Name(){
+    return(
+        <div className="name-div">
+        <span className="Name">Aditya </span>
+        <span className="Name">Kumar </span>
+        <span className="Name">Mathur</span>
+    </div>
+    );
+}
+
+export default Name;
